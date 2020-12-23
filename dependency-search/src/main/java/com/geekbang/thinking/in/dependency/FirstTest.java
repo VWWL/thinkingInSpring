e792709f642f38c09868e2e9279f16c8cc1154f3
王唯力 <897236293@qq.com>
2020-12-23 08:50:33 +0800

    update dependency-search/src/main/java/com/geekbang/thinking/in/dependency/FirstTest.java.

--- dependency-search/src/main/java/com/geekbang/thinking/in/dependency/FirstTest.java
+++ dependency-search/src/main/java/com/geekbang/thinking/in/dependency/FirstTest.java
@@ -2,6 +2,6 @@ package com.geekbang.thinking.in.dependency;
 
 public class FirstTest {
     public static void main(String[] args) {
-        System.out.println("嘿嘿嘿嘿");
+        System.out.println("吼吼吼");
     }
 }
